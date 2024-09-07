2fa4e640f4b28bda0838eafb1d236bac53045685
Ammar <muhammad2000ammar@gmail.com>
2024-09-07 23:25:31 +0200

    unittest

---
+++ unit_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestXXXX(unittest.TestCase):
+
+    def test_xxxxxxx(self):
+
+        data = [100, 200, 300]
+        result = sum(data)
+        self.assertEqual(result, 600)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
